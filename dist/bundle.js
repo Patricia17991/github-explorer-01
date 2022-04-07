f535f23f5a1235f6905d56baff6d16a9f9a1f0d6
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-04-07 11:37:42 -0300

    Create bundle.js

---
+++ dist/bundle.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+//configurando o reactjs
+function App() {
+  return /*#__PURE__*/_react["default"].createElement("h1", null, "Hello World!");
+}
+//cada linha html é convertida para uma função JS que o browser consiga entender.
+
+//aqui já temos a nossa estrutura do babel para consguirmos trabalhar com JS e com o React de uma maneira mais moderna.
