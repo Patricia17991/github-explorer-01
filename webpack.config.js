1441150cc13ff783317e763cc61de00d3f4894c6
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-04-08 07:56:26 -0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path'); //o node só entende esse formato de importação usando o require.
 
 module.exports = {
-    entry:'src/index.jsx', //agora vamos usar esse caminho usando o path
+    entry: path.resolve(__dirname,'src', 'index.jsx') //agora vamos usar esse caminho usando o path
 };  //onde colocamos o arquivo inicial da nossa aplicação
