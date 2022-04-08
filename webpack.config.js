180e375545ebdfefbe428de4e4450cefd87fbc7e
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-04-08 08:04:15 -0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -4,5 +4,6 @@ module.exports = {
     entry: path.resolve(__dirname,'src', 'index.jsx'), //agora vamos usar esse caminho usando o path.
     output:{
              path: path.resolve(__dirname, 'dist'), //falando qual o arquivo que vai ser gerado com o webpack.
+             filename: 'bundle.js'
     }
 };  //onde colocamos o arquivo inicial da nossa aplicação (isso é para o caminho receber a '/' correta de acordo com o sistema operacional).
