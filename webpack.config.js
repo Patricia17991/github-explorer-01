955c5be98e0c33275a10339e1407f2dac629baf0
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-04-08 07:51:33 -0300

    Add files via upload

---
+++ webpack.config.js
@@ -0,0 +1,5 @@
+const path = require('path'); //o node só entende esse formato de importação usando o require.
+
+module.exports = {
+    entry:'src/index.jsx', //agora vamos usar esse caminho usando o path
+};  //onde colocamos o arquivo inicial da nossa aplicação
