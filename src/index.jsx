1de3c1beade210c6f7e62eb55627840c071a8c04
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-04-14 12:54:25 -0700

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'; //quando dá erro pode ser só uma questão de importação
+//na versão 17 do react as importações do react não são necessárias para todos os arquivos que for usar ele
 import {render} from 'react-dom';
 import {App} from './App';
 
