bcb376acbe4b1eaf7d71bc6c535d5f699a4b4089
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-04-14 13:12:54 -0700

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,31 +1,23 @@
-const path = require('path'); //o node só entende esse formato de importação usando o require.
-
 module.exports = {
-    entry: path.resolve(__dirname, 'src', 'index.jsx'), //agora vamos usar esse caminho usando o path
-    //falando qual o arquivo que vai ser gerado com o webpack.
-    output: {
-        path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
-    },
+  mode: 'development',
+  entry: path.resolve(__dirname, 'src', 'index.jsx'),
+  output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: 'bundle.js'
+  },
 
-    //instrução resolve.
-    resolve: {
-        extensions:['.js', '.jsx'], //array com várias extensões de arquivos, pq por padrão ele só lê o .js.
-    },
+  resolve: {
+      extensions: ['.js', '.jsx'],
+  },
 
-    //configurando como nossa aplicação vai se comportar quando estivermos importando cada um dos tipos de arquivo.
-    module: {
-        //definindo propriedade rules.
-        rules: [
-            //array de regras
-            { //um objeto p/ cada tipo de arquivo.
-                //test recebe uma expressão regular para dizer se o arquivo é ou não js
-                test: /\.jsx$/,
-                exclude: /node_modules/, //excluindo o estiver no node_modules
-                use: 'babel-loader',
-                
+  module: {
+      rules: [
+          {
+             test: /\.jsx$/,
+             exclude: /node_modules/,
+             use: 'babel-loader',
+          }
+      ],
+  }
 
-            }
-        ],
-    }
-}; 
+};
