406c9b21bf0e72048d30ce949254f76a5e7a8fb9
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-04-18 08:03:35 -0700

    Update RepositoryList.jsx

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,11 +1,17 @@
 import { RepositoryItem } from "./RepositoryItem";
 
+const repository = {
+    name:'unform',
+    description:'Forms in React',
+    link:'https://github.com/unform/unform'
+}
+
 export function RepositoryList(){
     return(
         <section className="repository-list">
             <h1>Lista de Repositórios</h1>
             <ul>
-              <RepositoryItem repository="unform2" description="Forms in React" link="https://github.com/unform/unform"/>
+              <RepositoryItem repository={repository}/>
               <RepositoryItem/>
               <RepositoryItem/>
               <RepositoryItem/>
