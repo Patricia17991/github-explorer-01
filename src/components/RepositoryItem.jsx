b981ddf84e3678889eec1884a85eb5e3bf231ade
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-04-18 05:28:06 -0700

    Update RepositoryItem.jsx

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -1,3 +1,11 @@
 export function RepositoryItem(){
-    
+    return( //se eu for retornar só uma linha de html aí não preciso dos ().
+            <li>
+            <strong>unform</strong> 
+            <p>Forms in React</p>
+            <a href="">
+            Acessar o repositório
+            </a>
+            </li>
+    );
 }
