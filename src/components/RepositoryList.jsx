9bcb6a3c539ff060eb6b91f85625308a2253e6e8
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-04-18 04:46:21 -0700

    Update RepositoryList.jsx

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,1 +1,17 @@
-//
+export function RepositoryList(){
+    return(
+        //aqui vamos devolver uma listagem de repositórios
+        <section className="repository-list">
+            <h1>Lista de Repositórios</h1>
+            <ul>
+                <li>
+                    <strong>unform</strong> 
+                    <p>Forms in React</p>
+                    <a href="">
+                        Aceesar o repositório
+                    </a>
+                </li>
+            </ul>
+        </section>
+    );
+}
