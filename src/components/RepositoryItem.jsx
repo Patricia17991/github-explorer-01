9edb8cec6a6beb8364c52ee07069501addc1e448
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-04-18 08:20:59 -0700

    Update RepositoryItem.jsx

--- src/components/RepositoryItem.jsx
+++ src/components/RepositoryItem.jsx
@@ -2,8 +2,8 @@ export function RepositoryItem(props){
     return( //se eu for retornar só uma linha de html aí não preciso dos ().
             <li>
             <strong>{props.repository.name ?? 'Default'}</strong> 
-            <p>Forms in React</p>
-            <a href="">
+            <p>{props.repository.description}</p>
+            <a href={props.repository.link}>
             Acessar o repositório
             </a>
             </li>
