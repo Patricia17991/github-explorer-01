c589cf30170588fff8e03160ebe27ba3bc284ec9
Patricia Lima Felismino <72756164+Patricia17991@users.noreply.github.com>
2022-04-20 06:08:33 -0700

    Update RepositoryList.jsx

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -5,13 +5,6 @@ import '../styles/repositories.scss'
 // https://api.github.com/orgs/rocketseat/repos
 //por padrão só 21 repositórios vão ser listados por conta da paginção, mas nós conseguimos mostrar mais 
 
-
-const repository = {
-    name:'unform',
-    description:'Forms in React',
-    link:'https://github.com/unform/unform'
-}
-
 export function RepositoryList(){
     const[repositories, setRepositories] = useState([]);  //criando um estado para armazenar a listagem de repositórios
 
@@ -25,6 +18,7 @@ export function RepositoryList(){
         <section className="repository-list">
             <h1>Lista de Repositórios</h1>
             <ul>
+              {repositories}
               <RepositoryItem repository={repository}/>
               <RepositoryItem repository={repository}/>
               <RepositoryItem repository={repository}/>
